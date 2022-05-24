f4e596b3000f194c3ee7795a2b740a12ab9b961a
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-05-24 22:18:31 +0300

    first commit

---
+++ src/test/java/first_test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class first_test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
